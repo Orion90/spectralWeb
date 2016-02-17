6d8fa4347cc7f042d100b2cce0522c8e05c4027d
Mikael Gustafsson <micke@youngskilled.com>
2016-02-17 11:23:16 +0100

    Removed a LOT of shit

--- Canvaschart.jsx
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-'use strict'
-var React = require('react');
-var CanvasChart = React.createClass({
-
-});
-
-module.export = CanvasChart;
--- index.jsx
+++
@@ -1,7 +0,0 @@
-/** @jsx React.DOM */
-'use strict'
-var React = require('react');
-var Spectrum = require('./Spectrum');
-var ReactDOM = require('react-dom');
-
-ReactDOM.render(<Spectrum />, document.getElementById('content'))
